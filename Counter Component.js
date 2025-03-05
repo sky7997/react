c0d2c2572cb6bcd00457a6162e6281b37be4debc
sky7997 <skyasarthetwothrones@gmail.com>
2025-03-05 15:50:19 +0530

    Create Counter Component.js

---
+++ Counter Component.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h2>Counter: {count}</h2>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+      <button onClick={() => setCount(0)}>Reset</button>
+    </div>
+  );
+};
+
+export default Counter;
